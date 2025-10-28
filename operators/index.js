ae50d9f2569510c491cf20c44b4f0bf31f5fcb76
Joseph <joshlink843@gmail.com>
2025-10-28 16:11:34 +0300

    added operator examples in Javascript

---
+++ operators/index.js
@@ -0,0 +1,24 @@
+// Arithmetic Operators
+10 % 3
+let a = 15;
+let b = 7;
+
+console.log(a + b);
+console.log(a % b);
+console.log(a++);
+console.log(a);
+
+for (let a = 10; a > 0; a--) {
+  console.log(a);
+}
+
+// Assignment Operator
+let x = 5;
+x = 15;
+
+let total = 0;
+for (let i = 1; i <= 5; i++) {
+  total += i;
+}
+console.log(total);
+
---
+++ operators/operators_examples.js
@@ -0,0 +1,44 @@
+// Operators    
+//quick overview of different types of operators in JavaScript
+// 1. Arithmetic operators
+Arithmetic operators
+Logical Operators
+Comparison (Relational) Operators
+Unary Operators
+Ternary Operators 
+Bitwise Operators
+String Operators
+Type operators
+Nullish (Coalescing) and optional chaining operators
+
+// Aithmetic operators
+Modulus operator
+10 % 3
+
+let a = 15;
+let b = 7;
+
+console.log(a + b);
+console.log(a % b);
+console.log(a++);
+console.log(a);
+
+for (let a = 0; a < 10; a++ ){
+	console.log(a);
+}
+
+// Assignement Operator
+let x = 5;
+x = 15
+
+let total = 0;
+for (let i = 1; i<=5; i++) {
+    total += i;
+}
+console.log(total);
+
+1            1            total=0+1           1
+2            2            1+2                 3
+3            3            3+3                  6
+4            4             6+4                   10
+5            5             10 +5                  15
